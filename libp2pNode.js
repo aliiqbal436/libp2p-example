6f5ea01991def18885febc9bc919fea45370363f
Ali Iqbal <ali.iqbal@invozone.com>
2023-06-16 16:20:51 +0500

    added peer creation and bootstrap link

--- libp2pNode.js
+++ libp2pNode.js
@@ -4,14 +4,32 @@ import { tcp } from "@libp2p/tcp";
 import { mplex } from "@libp2p/mplex";
 import { yamux } from "@chainsafe/libp2p-yamux";
 import { noise } from "@chainsafe/libp2p-noise";
-import { createEd25519PeerId } from "@libp2p/peer-id-factory";
+import { createEd25519PeerId, exportToProtobuf } from "@libp2p/peer-id-factory";
+import { peerIdFromKeys } from '@libp2p/peer-id'
 import { kadDHT } from '@libp2p/kad-dht'
+import fs from 'fs';
+import { buffer } from "stream/consumers";
 
-const peerId = await createEd25519PeerId();
-console.log(peerId);
+if(!fs.existsSync('config.json')) {
+  const peerId = await createEd25519PeerId();
+  const peerIdObject = {
+    id: peerId.toString(),
+    privateKey: peerId.privateKey,
+    publicKey: peerId.publicKey,
+  }
+  
+  const peerIdJson = JSON.stringify(peerIdObject, null, 2);
+  fs.writeFileSync('config.json', peerIdJson);
+}
 
+const peerIdConfig = fs.readFileSync('config.json', 'utf8');
+const peerIdObject = JSON.parse(peerIdConfig);
+// console.log('peerIdKeys ====', Buffer.from(peerIdObject.publicKey.data));
+
+const peerIdKeys = await peerIdFromKeys(Buffer.from(peerIdObject.publicKey.data), Buffer.from(peerIdObject.privateKey.data))
+console.log('peerIdKeys ====', peerIdKeys);
 const node = await createLibp2p({
-  peerId,
+  peerId: peerIdKeys,
   addresses: {
     // To signal the addresses we want to be available, we use
     // the multiaddr format, a self describable address
@@ -24,10 +42,23 @@ const node = await createLibp2p({
     peerDiscovery: [
         bootstrap({
         interval: 60e3,
-        list: ['/ip4/182.191.70.215/tcp/4001']
+        list: ['/ip4/182.191.70.215/tcp/4001/p2p/12D3KooWEiUjPkRtEJ2tfkRSH7eYKvCPdCf64FFLSN1C9ce6rXoj']
         })
     ]
 });
 
-node.start();
-console.log("node.getPeers() =====", node.getMultiaddrs());
+
+node.addEventListener('peer:connect', (evt) => {
+  console.log('Connection established to:', evt.detail.remotePeer.toString())	// Emitted when a new connection has been created
+})
+
+node.addEventListener('peer:discovery', (evt) => {
+  // No need to dial, autoDial is on
+  console.log('Discovered:', evt.detail.id.toString())
+})
+
+await node.start()
+
+// /ip4/182.191.70.215/tcp/4001/p2p/12D3KooWEiUjPkRtEJ2tfkRSH7eYKvCPdCf64FFLSN1C9ce6rXoj
+// node.start();
+console.log("node.getPeers() =====", node.getPeers());
